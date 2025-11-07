65fe4fb080a051980b7d9f499373598780b7f847
Jeff-da-furry <jeffrey-w-holtman@outlook.com>
2025-11-07 17:35:00 -0600

    initial commit

--- core/modules/git.nix
+++ core/modules/git.nix
@@ -1,10 +1,24 @@
-{ config, packages, ... }:
+{ config, lib , pkgs, ... }:
 
 {
-  services.openssh = {
+  programs.git = {
     enable = true;
+    package =pkgs.gitFull;
+
+    config = {
+      user.name = "Jeff-da-furry";
+      user.email = "jeffrey-w-holtman@outlook.com";
+      init.defaultBranch = "main";
+      core.editor = "nvim";
+      pull.rebase = true;
+    };
   };
 
   programs.ssh.startAgent = true;
-  programs.git.enable = true;
+  environment.variables.SSH_AUTH_SOCK = "/run/user/1000/ssh-agent";
+
+  security.sudo.extraConfig = ''
+    Defaults env_keep += "SSH_AUTH_SOCK"
+  '';
+
 }
